0f1b6b34a9150d919c7d8c459e396648efeb3f58
kenwest <ken.west@internode.on.net>
2016-06-28 18:30:02 +1000

    Indicate mandatory fields via input's placeholder

--- template.php
+++ template.php
@@ -15,15 +15,15 @@ function gpbootstrap_add_placeholders_to_inputs($rendered) {
    * Add placeholders to inputs
    */
   $inputs = array (
-    'first_name' => 'First name',
-    'last_name' => 'Last name',
-    'email-6' => 'Email address',
-    'phone-6-2' => 'Mobile',
-    'custom_44' => 'Additional information',
-    'street_address-5' => 'Street address',
+    'first_name' => 'First name *',
+    'last_name' => 'Last name *',
+    'email-6' => 'Email address *',
+    'phone-6-2' => 'Mobile *',
+    'custom_44' => 'Additional information, eg names of guests, dietary requirements',
+    'street_address-5' => 'Street address *',
   	'supplemental_address_1-5' => 'Additional address',
-    'city-5' => 'Suburb',
-    'postal_code-5' => 'Postcode',
+    'city-5' => 'Suburb *',
+    'postal_code-5' => 'Postcode *',
   );
   $patterns = $replacements = array();
   foreach ($inputs as $from => $to) {
