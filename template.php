4fe02fa03410501e4740aa3709fb5f40c04f1b62
kenwest <ken.west@internode.on.net>
2016-08-02 01:33:56 +1000

    Prepend output with a DIV which requests permission to use cookies. This solves a problem where Safari only allows third-party cookies in an IFRAME when that site has been previously visited (not in an IFRAME) and has set cookies. See http://labs.fundbox.com/third-party-cookies-with-ie-at-2am/ for more information.

--- template.php
+++ template.php
@@ -13,7 +13,7 @@ function gpbootstrap_preprocess_html(&$variables, $hook) {
 /**
  * Add placeholders to input elements
  */
-function gpbootstrap_add_placeholders_to_inputs($rendered) {
+function gpbootstrap_update_civicrm_event_registration_page($rendered) {
   /*
    * Only update CiviCRM pages.
    */
@@ -42,10 +42,77 @@ function gpbootstrap_add_placeholders_to_inputs($rendered) {
   }
   $updated = preg_replace($patterns, $replacements, $rendered);
 
+  /*
+   * Prepend output with a DIV which requests permission to use cookies.
+   * Solves a problem where Safari only allows third-party cookies in an
+   * IFRAME when that site has been previously visited (not in an IFRAME)
+   * and has set cookies.
+   *
+   * See http://labs.fundbox.com/third-party-cookies-with-ie-at-2am/ for
+   * more information.
+   */
+  $cookiesPermission = '
+<div class="gpb-cookie-permissions gpb-cookie-hidden" id="gpb-cookie-permissions">
+  <div class="gpb-cookie-description">
+    Registering for this event requires the use of cookies. Please click here to accept the use of cookies.
+  </div>
+  <div class="gpb-cookie-button-area">
+    <span class="gpb-cookie-button" id="gpb-cookie-button">
+      <i class="fa-check"></i>
+      <span class="gpb-cookie-accept">Accept</span>
+    </span>
+  </div>
+  <div class="gpb-cookie-description">
+    If you have technical issues with this form, you can register at the ' .
+    '<a style="display: inline-block;" href="https://citybibleforum.org' .
+    request_uri() .
+    '"><strong>City Bible Forum</strong></a> site.
+  </div>
+</div>
+
+<script type="text/javascript">' . "
+CRM.$(function($) {
+  function setGpbCookie(cname, cvalue) {
+    document.cookie = cname + '=' + cvalue;
+  }
+
+  function getGpbCookie(cname) {
+    var name = cname + '=';
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return '';
+  }
+
+  test_cookie = setGpbCookie('gpb_test_cookie', 'test');
+  if (getGpbCookie('gpb_test_cookie') == '') {
+    $('#gpb-cookie-permissions').removeClass('gpb-cookie-hidden');
+
+    $('#gpb-cookie-button').on('mousedown',function(e){
+      $('#gpb-cookie-button').addClass('gpb-cookie-loading');
+      e.preventDefault();
+      window.open(
+        '/sites/gpb/themes/gpbootstrap/set-cookie-then-close.html',
+        '_blank',
+        'width=20,height=20,left=' + screen.width + ',top=' + screen.height
+      );
+    });
+  }
+});
+" . '
+</script>';
+
   if (isset($updated)) {
-    return $updated;
+    return $cookiesPermission . $updated;
   }
   else {
-    return $rendered;
+    return $cookiesPermission . $rendered;
   }
 }
